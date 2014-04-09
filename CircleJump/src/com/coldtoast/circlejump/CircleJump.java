bcf5d04a57a51201343b37cdd3dd699e7555b3a0
mccordnate <mccordnate@gmail.com>
2014-04-09 16:03:25 -0500

    High score + Stars + gravity/speed simultaneous increase + beginnings of outfit implementation

--- CircleJump/src/com/coldtoast/circlejump/CircleJump.java
+++ CircleJump/src/com/coldtoast/circlejump/CircleJump.java
@@ -2,6 +2,7 @@ package com.coldtoast.circlejump;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -23,15 +24,20 @@ public class CircleJump implements ApplicationListener {
 	private SpriteBatch batch;
 	private ShapeRenderer shapeRenderer;
 	private BitmapFont font;
+	private BitmapFont highFont;
+	public static Preferences settings;
 	
 	Hank hank;
 	Block block;
 	MenuButton play;
 	MenuButton logo;
+	BackgroundSpinner bs1;
+	BackgroundSpinner bs2;
+	BackgroundSpinner bs3;
 	FreeTypeFontGenerator gen;
 	FreeTypeFontParameter par;
 	Sound jump;
-	Sound hurt;
+	static Sound hurt;
 	Sound portal;
 	Sound portalOpen;
 	Sound portalClose;
@@ -41,6 +47,7 @@ public class CircleJump implements ApplicationListener {
 	double rotationSpeed = 2;
 	boolean canScore = false;
 	double resetSpeed = 0;
+	static float gravity = 0;
 	
 	boolean openPortal = false;
 	int portalSize = 0;
@@ -49,7 +56,7 @@ public class CircleJump implements ApplicationListener {
 	boolean fontFadeIn = false;
 	float fontAlpha = 0;
 	
-	int currentScore = 0;
+	static int currentScore = 0;
 	
 	@Override
 	public void create() {				
@@ -73,10 +80,20 @@ public class CircleJump implements ApplicationListener {
 		font.setColor(0,1,0,fontAlpha);
 		font.setScale(1,-1);
 		
+		highFont = gen.generateFont(80);
+		highFont.getRegion().getTexture().setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		highFont.setColor(.9F, .1F,0,1);
+		highFont.setScale(1,-1);
+		
 		hank = new Hank();
 		block = new Block(508,850);
-		play = new MenuButton("play.png", 540, 960);
+		play = new MenuButton("squarePlayBig.png", 540, 960);
 		logo = new MenuButton("logo.png", 540, 260);
+		bs1 = new BackgroundSpinner();
+		bs2 = new BackgroundSpinner();
+		bs3 = new BackgroundSpinner();
+		
+		settings = Gdx.app.getPreferences("settings");
 	}
 
 	
@@ -88,6 +105,7 @@ public class CircleJump implements ApplicationListener {
 		
 		camera.update();
 		
+		//set block position
 		block.bounds.setPosition((float)(Math.sin(Math.toRadians(rotation))*380 + 508), (float)(-Math.cos(Math.toRadians(rotation))*380 + 928));
 		if(rotation+rotationSpeed >= 360){
 			rotation = rotation+rotationSpeed - 360;
@@ -95,123 +113,52 @@ public class CircleJump implements ApplicationListener {
 			rotation += rotationSpeed;
 		}
 		
+		
+		//Play has been clicked. Game intro initiate
 		if(play.fadeOut){
-			if(play.alpha - 0.02F > 0){
-				play.image.setAlpha(play.alpha-0.02F);
-				play.alpha -= 0.02F;
-				logo.image.setAlpha(logo.alpha-0.02F);
-				logo.alpha-=0.02F;
-			} else {
-				play.alpha = 0;
-				play.image.setAlpha(0);
-				logo.alpha = 0;
-				logo.image.setAlpha(0);
-				play.fadeOut = false;
-				openPortal = true;
-				portalOpen.play(0.4F);
-			}
+			fadeOutPlay();
 		}
-		
 		if(openPortal){
-			if(!(portalSize + 2 >= 100)){
-				portalSize += 2;
-			} else {
-				hank.fadeIn = true;
-				openPortal = false;
-			}
-			
-			shapeRenderer.setProjectionMatrix(camera.combined);
-			 
-			shapeRenderer.begin(ShapeType.Filled);
-			shapeRenderer.setColor(1,1,1,1);
-			shapeRenderer.rect(590 - portalSize,910,portalSize,100);
-			shapeRenderer.end();
+			openPortalFunction();
 		}
-		
 		if(hank.fadeIn){
-			
-			shapeRenderer.setProjectionMatrix(camera.combined);
-			 
-			shapeRenderer.begin(ShapeType.Filled);
-			shapeRenderer.setColor(1,1,1,1);
-			shapeRenderer.rect(490,910,100,100);
-			shapeRenderer.end();
-			
-			if(hank.alpha + 0.02F < 1F){
-				hank.image.setAlpha(hank.alpha+0.02F);
-				hank.alpha+=0.02F;
-			} else {
-				hank.alpha = 1F;
-				hank.image.setAlpha(1F);
-				if(rotation > 200 && rotation < 340){
-					hank.fadeIn = false;
-					inSession = true;
-					closePortal = true;
-					portalClose.play(0.4F);
-				}
-			}
+			fadeHankIn();
 		}
-		
 		if(closePortal){
-			if(!(portalSize - 2 <= 0)){
-				portalSize -= 2;
-			} else {
-				closePortal = false;
-				fontFadeIn = true;
-			}
-			
-			shapeRenderer.setProjectionMatrix(camera.combined);
-			 
-			shapeRenderer.begin(ShapeType.Filled);
-			shapeRenderer.setColor(1,1,1,1);
-			shapeRenderer.rect(490,910,portalSize,100);
-			shapeRenderer.end();
+			closePortalFunction();
 		}
-		
 		if(fontFadeIn){
-			if(fontAlpha + 0.01F < 1F){
-				fontAlpha += 0.01F;
-				font.setColor(0, 1, 0, fontAlpha);
-				portal.setLooping(portalId, false);
-			} else {
-				fontAlpha = 1F;
-				font.setColor(0,1,0,fontAlpha);
-				fontFadeIn = false;
-			}
+			fadeFontIn();
 		}
 		
+		
+		//Hank has died. Fade back play button
 		if(play.fadeIn){
-			if(play.alpha + 0.02F < 1F){
-				play.image.setAlpha(play.alpha+0.02F);
-				play.alpha+=0.02F;
-				logo.image.setAlpha(logo.alpha+0.02F);
-				logo.alpha+=0.02F;
-				rotationSpeed += resetSpeed;
-			} else {
-				play.alpha = 1F;
-				play.image.setAlpha(1F);
-				logo.alpha = 1F;
-				logo.image.setAlpha(1F);
-				play.fadeIn = false;
-				hank.image.setRotation(0);
-				hank.bounds.set(508, 928, 64, 64);
-				hank.image.setAlpha(0);
-				inSession = false;
-				hank.dead = false;
-				font.setColor(0, 1, 0, 0);
-				currentScore = 0;
-				rotationSpeed = 2;
-				resetSpeed = 0;
-			}
+			fadePlayIn();
 		}
 		
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		hank.image.setPosition(hank.bounds.x, hank.bounds.y);
-		hank.image.draw(batch);
+		bs1.image.rotate(0.23F);
+		bs2.image.rotate(0.05F);
+		bs3.image.rotate(0.1F);
+		bs1.image.setAlpha(0.2F);
+		bs2.image.setAlpha(0.15F);
+		bs3.image.setAlpha(0.25F);
+		bs1.image.setPosition(-200, -250);
+		bs2.image.setPosition(800, 500);
+		bs3.image.setPosition(0, 1400);
+		bs1.image.draw(batch);
+		bs2.image.draw(batch);
+		bs3.image.draw(batch);
+		//hank.image.setPosition(hank.bounds.x, hank.bounds.y);
+		//hank.image.draw(batch);
+		batch = hank.draw(batch);
 		block.image.setPosition(block.bounds.x, block.bounds.y);
 		block.image.draw(batch);
 		font.draw(batch, Integer.toString(currentScore), 480, 850);
+		highFont.draw(batch, "High Score: " + settings.getInteger("highScore", 0), 220,1650);
+		highFont.draw(batch, "Stars: " + settings.getInteger("stars", 0), 280, 1750);
 		play.image.draw(batch);
 		logo.image.draw(batch);
 		batch.end();
@@ -223,21 +170,22 @@ public class CircleJump implements ApplicationListener {
 		shapeRenderer.circle(540, 960, 450);
 		shapeRenderer.end();
 		
+		
 		if(inSession){
 			if(rotation > 0 && rotation < 190){
 				canScore = true;
 			}
 			
+			//check if Hank hit block
 			if(!hank.dead && hank.bounds.overlaps(block.bounds)){
-				hank.dead = true;
-				hurt.play(0.5F);
-				hank.image.setRotation(-45);
-				hank.v.x = -20;
+				hank.die();
+				
 			}
 			
+			//If Hank isn't dead, check his jumping speed etc.
 			if(!hank.dead){
 				if(hank.bounds.y + hank.v.y < 1344){
-					hank.v.y += 1;
+					hank.v.y += 2.3F + gravity;
 				} else	{
 					hank.v.y = 0;
 					hank.bounds.y = 1344;
@@ -264,15 +212,129 @@ public class CircleJump implements ApplicationListener {
 				jump.play(0.3F);
 			}
 			
-			if(!hank.dead && rotation > 200 && canScore){
+			if(!hank.dead && rotation > 210 && canScore){
 				currentScore += 1;
 				rotationSpeed += 0.15;
+				gravity += 0.1F;
 				canScore = false;
 			}
 		} else {
 			if(play.isClicked(camera)){
 				play.fadeOut = true;
 			}
+		}
+	}
+	
+	public void fadeOutPlay(){
+		if(play.alpha - 0.02F > 0){
+			play.image.setAlpha(play.alpha-0.02F);
+			play.alpha -= 0.02F;
+			logo.image.setAlpha(logo.alpha-0.02F);
+			logo.alpha-=0.02F;
+			highFont.setColor(.9F, .1F, 0, play.alpha);
+		} else {
+			play.alpha = 0;
+			play.image.setAlpha(0);
+			logo.alpha = 0;
+			logo.image.setAlpha(0);
+			highFont.setColor(.9F, .1F,0,0);
+			play.fadeOut = false;
+			openPortal = true;
+			portalOpen.play(0.4F);
+		}
+	}
+	
+	public void openPortalFunction(){
+		if(!(portalSize + 2 >= 100)){
+			portalSize += 2;
+		} else {
+			hank.fadeIn = true;
+			openPortal = false;
+		}
+		
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		 
+		shapeRenderer.begin(ShapeType.Filled);
+		shapeRenderer.setColor(1,1,1,1);
+		shapeRenderer.rect(590 - portalSize,910,portalSize,100);
+		shapeRenderer.end();
+	}
+	
+	public void fadeHankIn(){
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		 
+		shapeRenderer.begin(ShapeType.Filled);
+		shapeRenderer.setColor(1,1,1,1);
+		shapeRenderer.rect(490,910,100,100);
+		shapeRenderer.end();
+		
+		if(hank.alpha + 0.02F < 1F){
+			hank.image.setAlpha(hank.alpha+0.02F);
+			hank.alpha+=0.02F;
+		} else {
+			hank.alpha = 1F;
+			hank.image.setAlpha(1F);
+			if(rotation > 200 && rotation < 340){
+				hank.fadeIn = false;
+				inSession = true;
+				closePortal = true;
+				portalClose.play(0.4F);
+			}
+		}
+	}
+	
+	public void closePortalFunction(){
+		if(!(portalSize - 2 <= 0)){
+			portalSize -= 2;
+		} else {
+			closePortal = false;
+			fontFadeIn = true;
+		}
+		
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		 
+		shapeRenderer.begin(ShapeType.Filled);
+		shapeRenderer.setColor(1,1,1,1);
+		shapeRenderer.rect(490,910,portalSize,100);
+		shapeRenderer.end();
+	}
+	
+	public void fadeFontIn(){
+		if(fontAlpha + 0.01F < 1F){
+			fontAlpha += 0.01F;
+			font.setColor(0, 1, 0, fontAlpha);
+			portal.setLooping(portalId, false);
+		} else {
+			fontAlpha = 1F;
+			font.setColor(0,1,0,fontAlpha);
+			fontFadeIn = false;
+		}
+	}
+	
+	public void fadePlayIn(){
+		if(play.alpha + 0.02F < 1F){
+			play.image.setAlpha(play.alpha+0.02F);
+			play.alpha+=0.02F;
+			logo.image.setAlpha(logo.alpha+0.02F);
+			logo.alpha+=0.02F;
+			highFont.setColor(.9F, .1F, 0, play.alpha);
+			rotationSpeed += resetSpeed;
+		} else {
+			play.alpha = 1F;
+			play.image.setAlpha(1F);
+			logo.alpha = 1F;
+			logo.image.setAlpha(1F);
+			highFont.setColor(.9F, .1F, 0, 1);
+			play.fadeIn = false;
+			hank.image.setRotation(0);
+			hank.bounds.set(508, 928, 64, 64);
+			hank.image.setAlpha(0);
+			inSession = false;
+			hank.dead = false;
+			font.setColor(0, 1, 0, 0);
+			currentScore = -1;
+			rotationSpeed = 2;
+			resetSpeed = 0;
 		}
 	}
 	
--- CircleJump/src/com/coldtoast/circlejump/Hank.java
+++ CircleJump/src/com/coldtoast/circlejump/Hank.java
@@ -3,25 +3,35 @@ package com.coldtoast.circlejump;
 import java.util.Vector;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Hank {
 	private final Texture texture;
+	private final Texture hatTexture;
 	public final Sprite image;
+	private final Sprite hat;
 	public final Rectangle bounds;
 	public final Vector2 v;
 	public boolean dead;
 	public boolean fadeIn = false;
 	public float alpha = 0;
+	private static Preferences outfit = Gdx.app.getPreferences("outfit");
 
 	public Hank(){
 		texture = new Texture(Gdx.files.internal("hank.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		hatTexture = new Texture(Gdx.files.internal("clothes/hats/" + getHat()));
+		hatTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		image = new Sprite(texture);
+		hat = new Sprite(hatTexture);
+		hat.flip(false, true);
 		image.flip(false, true);
 		//bounds = new Rectangle(508,1344,64,64); //This is Hank's position for gameplay start
 		bounds = new Rectangle(508,928,64,64); //This is Hank's position for intro
@@ -33,6 +43,38 @@ public class Hank {
 	}
 	
 	public void jump(){
-		v.y -= 20;
+		v.y -= 32 + (CircleJump.gravity * 7);
+	}
+	
+	public SpriteBatch draw(SpriteBatch batch){
+		image.setPosition(bounds.x, bounds.y);
+		image.draw(batch);
+		hat.setPosition(bounds.x, bounds.y);
+		hat.draw(batch);
+		
+		return batch;
+	}
+	
+	public void die(){
+		dead = true;
+		CircleJump.gravity = 0;
+		CircleJump.hurt.play(0.5F);
+		image.setRotation(-45);
+		hat.setRotation(-45);
+		v.x = -20;
+		if(CircleJump.settings.getInteger("highScore") < CircleJump.currentScore){
+			CircleJump.settings.putInteger("highScore", CircleJump.currentScore);
+		}
+		CircleJump.settings.putInteger("stars", CircleJump.settings.getInteger("stars") + CircleJump.currentScore / 5);
+		CircleJump.settings.flush();
+	}
+	
+	public String getHat(){
+		if(outfit.getInteger("hat", 0) == 1){
+			return "headband.png";
+		}
+		else{
+			return "none.png";
+		}
 	}
 }
